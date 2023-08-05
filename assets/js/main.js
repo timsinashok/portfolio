7a17fc91d319e897844204bca37f1388e7eaf481
Ashok Timsina <ashoktimsina147181@gmail.com>
2023-08-05 00:26:48 -0400

    updated home

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+/*
+ * Dew Templates (Personal Portofolio)
+ * Created by : vavelo
+ * Version : 1.1
+ */
+
+
+/* INDEX OF CONTENTS JAVASCRIPT
+==================================================
+  XX. PRELOADER
+  01. NAVIGATION
+  02. MOBILE NAVIGATION
+  03. PARALLAX BACKGROUND EFFECT
+  04. TEXT ROTATE
+  05. HOME ARROW DOWN
+  06. SMOTH SCROLL
+  07. PORTFOLIO FILTER IMAGE
+  08. TESTIMONIAL SLIDER
+  09. ANIMATION EFFECT  
+*/
+
+
+(function ($) {
+	'use strict';
+
+	jQuery(document).ready(function () {
+
+
+		/* XX. PRELOADER
+		==================================================*/
+
+		$(window).on('load', function () {
+			$("#status").fadeOut();
+			$("#preloader").delay(500).fadeOut("slow");
+		});
+
+
+		/* 01. NAVIGATION
+		==================================================*/
+
+		$(window).on('scroll', function () {
+			if ($(window).scrollTop() > 100) {
+				$('#navigation').addClass('nav-bg');
+			} else {
+				$('#navigation').removeClass('nav-bg');
+			}
+		});
+
+
+		/* 02. MOBILE NAVIGATION
+		==================================================*/
+
+		$(document).on('click', '.navbar-collapse.in', function (e) {
+			if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+				$(this).collapse('hide');
+			}
+		});
+
+		$('body').scrollspy({
+			target: '.navbar-collapse',
+			offset: 195
+		});
+
+
+		/* 03. PARALLAX BACKGROUND EFFECT
+		==================================================*/
+
+		var parallax = function () {
+			$(window).stellar();
+		};
+
+		$(function () {
+			parallax();
+		});
+
+
+		/* 04. TEXT ROTATE
+		==================================================*/
+		$(document).on('ready', function () {
+			$('#text-rotator').rotatetext({
+				fadeSpeed: 500,
+				pauseSpeed: 2000
+			});
+		});
+
+
+		/* 05. HOME ARROW DOWN
+		==================================================*/
+
+		var arrowBounce = function () {
+			var arrow = $(".arrow");
+			if (arrow.hasClass("lift")) {
+				arrow.removeClass("lift");
+			} else {
+				arrow.addClass("lift");
+			}
+		};
+
+		setInterval(arrowBounce, 800);
+
+
+		/* 06. SMOTH SCROLL
+		==================================================*/
+
+		$('a.smoth-scroll').on('click', function (e) {
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 50
+			}, 1000);
+			e.preventDefault();
+		});
+
+
+		/* 07. PORTFOLIO FILTER IMAGE
+		==================================================*/
+		$('#port-image').mixItUp();
+
+
+		/* 08. TESTIMONIAL SLIDER
+		==================================================*/
+
+		$('.flexslider').flexslider({
+			animation: "fade",
+			directionNav: false
+		});
+
+
+		/* 09. ANIMATION EFFECT
+		==================================================*/
+
+		AOS.init({
+			duration: 1200,
+			once: true,
+			disable: 'mobile'
+		});
+
+
+
+	});
+
+})(jQuery);
---
+++ assets/php/mailer.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * This example shows how to handle a simple contact form safely.
+ */
+
+//Import PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+
+$msg = '';
+//Don't run this unless we're handling a form submission
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
+
+    require '../vendor/autoload.php';
+
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer();
+    //Send using SMTP to localhost (faster and safer than using mail()) – requires a local mail server
+    //See other examples for how to use a remote server such as gmail
+    $mail->isSMTP();
+    $mail->Host = 'localhost';
+    $mail->Port = 25;
+
+    //Use a fixed address in your own domain as the from address
+    //**DO NOT** use the submitter's address here as it will be forgery
+    //and will cause your messages to fail SPF checks
+    $mail->setFrom('from@example.com', 'First Last');
+    //Choose who the message should be sent to
+    //You don't have to use a <select> like in this example, you can simply use a fixed address
+    //the important thing is *not* to trust an email address submitted from the form directly,
+    //as an attacker can substitute their own and try to use your form to send spam
+    $addresses = [
+        // 'sales' => 'sales@example.com',
+        // 'support' => 'support@example.com',
+        'accounts' => 'deosambridhi@gmail.com',
+    ];
+    //Validate address selection before trying to use it
+    if (array_key_exists('dept', $_POST) && array_key_exists($_POST['dept'], $addresses)) {
+        $mail->addAddress($addresses[$_POST['dept']]);
+    } else {
+        //Fall back to a fixed address if dept selection is invalid or missing
+        $mail->addAddress('support@example.com');
+    }
+    //Put the submitter's address in a reply-to header
+    //This will fail if the address provided is invalid,
+    //in which case we should ignore the whole request
+    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
+        $mail->Subject = 'PHPMailer contact form';
+        //Keep it simple - don't use HTML
+        $mail->isHTML(false);
+        //Build a simple message body
+        $mail->Body = <<<EOT
+Email: {$_POST['email']}
+Name: {$_POST['name']}
+Message: {$_POST['message']}
+EOT;
+        //Send the message, check for errors
+        if (!$mail->send()) {
+            //The reason for failing to send will be in $mail->ErrorInfo
+            //but it's unsafe to display errors directly to users - process the error, log it on your server.
+            $msg = 'Sorry, something went wrong. Please try again later.';
+        } else {
+            $msg = 'Message sent! Thanks for contacting us.';
+        }
+    } else {
+        $msg = 'Invalid email address, message ignored.';
+    }
+}
+?>
